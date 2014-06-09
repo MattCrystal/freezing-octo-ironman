131f5aedfbdd620e5072d776f082588f736292a4
Matt Svihel <matt.svihel@gmail.com>
2014-06-09 14:37:59 -0500

    Halo50

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 19 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#19 SMP PREEMPT Mon Jun 9 11:22:28 CDT 2014"
+#define LINUX_COMPILE_BY "nineinchnails"
+#define LINUX_COMPILE_HOST "nothing-studios"
+#define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (Linaro GCC 4.9-2014.05) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.88-TeamNocturnal-NINexus-5-Halo50-PersistenceOfLoss"
